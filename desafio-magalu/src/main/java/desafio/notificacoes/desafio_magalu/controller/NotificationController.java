873cefe10ce2b73ae11564862015350eb7be2a89
Matheusgabriel1234 <magaje2005@gmail.com>
2024-07-26 13:39:59 -0300

    Get and Delete(cancel notification) method

--- desafio-magalu/src/main/java/desafio/notificacoes/desafio_magalu/controller/NotificationController.java
+++ desafio-magalu/src/main/java/desafio/notificacoes/desafio_magalu/controller/NotificationController.java
@@ -7,7 +7,17 @@ import desafio.notificacoes.desafio_magalu.dto.NotificationDTO;
 import desafio.notificacoes.desafio_magalu.entities.Notifications;
 import desafio.notificacoes.desafio_magalu.repository.NotificationRepository;
 import desafio.notificacoes.desafio_magalu.service.NotificationService;
+
+import java.util.Optional;
+
+import javax.management.RuntimeErrorException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -32,5 +42,23 @@ public ResponseEntity<Void> scheduleNotification(@RequestBody NotificationDTO dt
 	return ResponseEntity.accepted().build();
 }
 	
+
+@GetMapping("/{notificationId}")
+public ResponseEntity<Optional<Notifications>> getNotification(@PathVariable Long notificationId){
+var notification = serv.findById(notificationId);
+
+if(notification.isPresent()) {
+	return new ResponseEntity<>(notification,HttpStatus.OK);
+}else {
+	throw new RuntimeErrorException(null,"Id não encontrado");
+}
+}
+
+@DeleteMapping("/{notificationId}")
+public ResponseEntity<Void> deleteById(@PathVariable Long notificationId){
+serv.deleteById(notificationId);
+return ResponseEntity.noContent().build();
+	
+}
 	
 }
--- desafio-magalu/src/main/java/desafio/notificacoes/desafio_magalu/service/NotificationService.java
+++ desafio-magalu/src/main/java/desafio/notificacoes/desafio_magalu/service/NotificationService.java
@@ -1,8 +1,12 @@
 package desafio.notificacoes.desafio_magalu.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import desafio.notificacoes.desafio_magalu.dto.NotificationDTO;
+import desafio.notificacoes.desafio_magalu.entities.Notifications;
+import desafio.notificacoes.desafio_magalu.entities.Status;
 import desafio.notificacoes.desafio_magalu.repository.NotificationRepository;
 
 @Service
@@ -19,7 +23,21 @@ public void scheduleNotification(NotificationDTO notification) {
 }
 
 
+public Optional<Notifications> findById(Long notificationId){
+return repo.findById(notificationId);
+}
 
+public void deleteById(Long notificationId){
+var id = findById(notificationId);
+
+if(id.isPresent()) {
+	id.get().setStatus(Status.values.CANCELED.toStatus());
+	repo.save(id.get());
+}
+
+
+
+}
 
 
 
